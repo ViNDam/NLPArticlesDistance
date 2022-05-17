4bbb01597c8fc08bf4321d055b5076b1bcc260f5
ViNDam <105737426+ViNDam@users.noreply.github.com>
2022-05-17 11:48:40 -0500

    Add files via upload

---
+++ codes/distance.R
@@ -0,0 +1,92 @@
+#!/usr/bin/RScript
+# --------------------------------------------------------------------------
+# Author: Vi Dam
+# Version: 20201218
+# Goal: Calculate the distance between artiles
+#
+# The distance between artiles are calculated using Euclidean distance, angle distance and SVD distance
+# The distance among articles’ mesh terms is also calculated using cosine angle distance.
+#
+# “tdMatrix.txt” and “meshMatrix.txt” file will be used as inputs.
+# Then, the difference between
+# 1. articles’ cosine angle and mesh-term’s distance,
+# 2. articles’ SVD cosine angle and mesh-term’s distance
+# are calculated.
+#
+# Usage: Rscript distance.R path/meshMatrix.txt path/tdMatrix.txt
+# --------------------------------------------------------------------------
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least 2 argument: if not, return an error
+if (length(args)==0 | length(args)==1) {
+  stop("At least two argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==2) {
+  # default output file
+  #args[3] = "out.txt"
+}
+
+#------------- READ FILES-------------------------------------------------------
+mesh <- read.table(args[1], header=T, check.names=F, sep="\t", quote="")
+mesh <- mesh[,-(which(colSums(mesh)==0))]
+
+D <- read.table(args[2], header=T, check.names=F, sep="\t", quote="")
+ind <- match(colnames(mesh), colnames(D))
+D <- D[,ind]
+
+#------------- TERM-DOCUMENT MATRIX Distance calculation------------------------
+# ----- Euclidean distance ----------
+tD <- t(D)
+dist <- as.matrix(dist(tD, method="euclidean"))
+D.dist <- t(combn(colnames(dist), 2))
+D.dist <- data.frame(D.dist, dist=dist[D.dist])
+
+# ----- cosine angle distances-------
+D.dist$acos <- ""
+for (i in 1:nrow(D.dist)){
+  D.dist$acos[i] <- acos((tD[rownames(tD)==D.dist[i,1],] %*% tD[rownames(tD)==D.dist[i,2],]) / (norm(tD[rownames(tD)==D.dist[i,1],], "2") * norm(tD[rownames(tD)==D.dist[i,2],], "2")))
+}
+
+# ----- SVD distance-----------------
+D2 <- as.matrix(D)
+colnames(D2) <- NULL
+
+D.svd <- svd(D2)
+S <- diag(D.svd$d)
+
+# ----- Low rank approximation-------
+n <- 36
+DLowRank <- D.svd$u[,1:n] %*% S[1:n,1:n] %*% t(D.svd$v [ ,1:n])
+
+tDLowRank <- t(DLowRank)
+rownames(tDLowRank) <- rownames(tD)
+
+# ----- Distance between low rank articles and query-----
+D.dist$SVDLowRank <- ""
+for (i in 1:nrow(D.dist)){
+  D.dist$SVDLowRank[i] <- acos((tDLowRank[rownames(tDLowRank)==D.dist[i,1],] %*% tDLowRank[rownames(tDLowRank)==D.dist[i,2],]) / (norm(tDLowRank[rownames(tDLowRank)==D.dist[i,1],], "2") * norm(tDLowRank[rownames(tDLowRank)==D.dist[i,2],], "2")))
+}
+
+D.dist[,3:5] <- apply(D.dist[,3:5], 2, as.numeric)
+
+# ------------VALIDATE using MESH-TERMS MATRIX----------------------
+
+# ------ VSM Euclidean distance------
+tmesh <- t(mesh)
+dist <- as.matrix(dist(tmesh, method="euclidean"))
+mesh.dist <- t(combn(colnames(dist), 2))
+mesh.dist <- data.frame(mesh.dist, dist=dist[mesh.dist])
+
+# ------ angle distances--------
+mesh.dist$acos <- ""
+for (i in 1:nrow(mesh.dist)){
+  mesh.dist$acos[i] <- acos((tmesh[rownames(tmesh)==mesh.dist[i,1],] %*% tmesh[rownames(tmesh)==mesh.dist[i,2],]) / (norm(tmesh[rownames(tmesh)==mesh.dist[i,1],], "2") * norm(tmesh[rownames(tmesh)==mesh.dist[i,2],], "2")))
+}
+mesh.dist[,3:ncol(mesh.dist)] <- apply(mesh.dist[,3:ncol(mesh.dist)], 2, as.numeric)
+
+# ------ DIFFERENCES between cosine angle and DLowRank------
+tmp <- norm(as.matrix(mesh.dist$acos) - as.matrix(D.dist$dist), "F") / norm(as.matrix(D.dist$dist), "F") * 100
+print(paste("The distance between articles and mesh terms using Euclidean distance is", tmp, "percent."))
+tmp <- norm(as.matrix(mesh.dist$acos) - as.matrix(D.dist$acos), "F") / norm(as.matrix(D.dist$acos), "F") * 100
+print(paste("The distance between articles and mesh terms using cosine angle is", tmp, "percent."))
+tmp <- norm(as.matrix(mesh.dist$acos) - as.matrix(D.dist$SVDLowRank), "F") / norm(as.matrix(D.dist$SVDLowRank), "F") * 100
+print(paste("The distance between articles and mesh terms using SVD is", tmp, "percent."))
---
+++ codes/prep.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+#############################################
+# Author: Vi Dam
+# Version: 20201218
+# Goal: Extract tokens and meshTerm from the input cancersArticles.txt
+#
+# Usage: python prep.py path/cancersArticles.txt
+#############################################
+import sys
+import os
+import pandas as pd
+import scipy
+import string
+import numpy as np
+from numpy.linalg import norm
+import nltk
+from nltk import *
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+stopwords = nltk.corpus.stopwords.words('english')
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+
+# ----------------- Parse parameters  ---------------------------
+parser = argparse.ArgumentParser(description='Prep data')
+
+parser.add_argument("-f", required=True, default=True,
+                    help="Articles text file")
+
+args = parser.parse_args()
+
+if args.f is None:
+    parser.error('No action requested, add -f')
+    
+args = parser.parse_args()
+
+# ----------------- Read articles file  ---------------------------
+df = pd.read_csv(args.f, sep='\t')
+
+# ----------------- Remove non-numerical Year--------
+df.dropna(subset = ["Year"], inplace=True)
+df['Year'] = pd.to_numeric(df['Year'], errors='coerce')
+df.sort_values(by='Year', ascending=True, inplace=True)
+
+# ----------------- Filter Year ---------------------------
+# # Filter Year - from 2016------
+# df.dropna(subset=['Year'])
+# df.dropna(subset=['MeshTerm'])
+# df2 = df.copy()
+# df2 = df2[(df2.Year >= 2016)]
+# df2.fillna("", inplace=True) #374864 articles, 4 cols
+
+# Filter Year - from 2019-----
+df.dropna(subset=['Year'])
+df.dropna(subset=['MeshTerm'])
+df2 = df.copy()
+df2 = df2[(df2.Year >= 2019)]
+df2.fillna("", inplace=True) #542 articles, 4 cols
+
+
+#--------------------TERM-DOCUMENT MATRIX---------------------------------
+tokens = []
+for t in df2.Title:
+    tokens.append(word_tokenize(t)) #list of all 1118214 titles/sentences'tokens)
+
+# remove stop words, non alpha characters and words with less than 2 alphabet
+
+# convert lemmatize, combine root words. ie. cancers & cancer = cancer & cancer
+
+words = [lemmatizer.lemmatize(word) for word in words]
+
+for ind,title in enumerate(tokens):
+    #tokens[ind+1] = [w.lower() for w in title if w.isalpha()] #can't use, it removes anti-cancer
+    title = [w.lower() for w in title if w not in string.punctuation]
+    title = [w.lower() for w in title if len(w) > 2]
+    title = [lemmatizer.lemmatize(w) for w in title]
+    tokens[ind] = [w for w in title if w.lower() not in stopwords]
+
+#--------list of unique tokens of all titles--------
+terms = []
+for ind, i in enumerate(tokens):
+    #print(ind)
+    for w in i:
+        if w not in terms:
+            terms.append(w)
+
+#--------create term-document matrix-----------------
+tdMat = np.array([[0]*len(terms)]*len(tokens))
+for ind, i in enumerate(tokens):
+    inds = []
+    for w in i:
+        inds.append(terms.index(w))
+    inds.sort()
+    d = {x:inds.count(x) for x in inds}
+    tmp = [int(k) for k in d.keys()]
+    tmp1 = [int(k) for k in d.values()]
+    tdMat[ind,tmp] = tmp1
+
+tdMat = tdMat.transpose()
+tdMat = pd.DataFrame(data=tdMat, columns=df2['PMID'], index=terms)
+tdMat.to_csv("tdMatrix.txt", header=True, sep="\t",index=False)
+
+
+#--------------------MESH - TERMS MATRIX----------------------------------
+mTokens = []
+for t in df2.MeshTerm:
+    mTokens.append(word_tokenize(t))
+
+# remove stopwords, special characters
+for ind,title in enumerate(mTokens):
+    title = [w.lower() for w in title if w not in string.punctuation]
+    title = [lemmatizer.lemmatize(w) for w in title]
+    mTokens[ind] = [w for w in title if w.lower() not in stopwords]
+
+#-------- list of unique tokens of all mesh terms--------
+mTerms = []
+for ind, i in enumerate(mTokens):
+    #print(ind)
+    for w in i:
+        if w not in mTerms:
+            mTerms.append(w)
+
+# ------ create mesh term matrix---------------
+meshMat = np.array([[0]*len(mTerms)]*len(mTokens))
+for ind, i in enumerate(mTokens):
+    inds = []
+    for w in i:
+        inds.append(mTerms.index(w))
+    inds.sort()
+    d = {x:inds.count(x) for x in inds}
+    tmp = [int(k) for k in d.keys()]
+    tmp1 = [int(k) for k in d.values()]
+    meshMat[ind,tmp] = tmp1
+
+meshMat = meshMat.transpose()
+meshMat = pd.DataFrame(data=meshMat, columns=df2['PMID'], index=mTerms)
+meshMat.to_csv("meshMatrix.txt", header=True, sep="\t",index=False)
